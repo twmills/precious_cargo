ebb2b4a5d36df35182902d9ab348a09a4c7190c8
Theo Mills <theo.mills@peopleadmin.com>
2012-07-19 11:37:57 -0500

    Add tomdoc.

--- lib/precious_cargo/data.rb
+++ lib/precious_cargo/data.rb
@@ -1,7 +1,16 @@
 module PreciousCargo
 
+  # Public: A collection of methods to encrypt and decrypt data using
+  # a supplied secret.
   module Data
     class << self
+
+      # Public: Encrypt the supplied data using a secret string. Currently only supports AES 256 encryption.
+      # data    - The data to be encrypted.
+      # options - Hash of values used to encrypt the secret.
+      #           :secret - A secret string.
+      #
+      # Returns the AES encrypted data.
       def encrypt!(data, options = {})
         secret = options.delete(:secret)
 
@@ -11,6 +20,12 @@ module PreciousCargo
         Base64.encode64(cipher.update(data) + cipher.final)
       end
 
+      # Public: Decrypt the supplied data using a secret string. Currently only supports AES 256 encryption.
+      # data    - The data to be encrypted.
+      # options - Hash of values used to encrypt the secret.
+      #           :secret - A secret string.
+      #
+      # Returns the AES encrypted data.
       def decrypt!(encrypted_data, options = {})
         secret = options.delete(:secret)
         encrypted_data = Base64.decode64(encrypted_data)
